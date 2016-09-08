f00d0cb1cff919f2ed08cc11359d69e9c1627a18
shanshanglaohu <thatzhigang@outlook.com>
2016-09-08 19:36:05 +0800

    create abstracts.py add BaseAbstractModel add BaseTagAbstractModel

--- notebooks/models/abstracts..py
+++ notebooks/models/abstracts..py
@@ -3,3 +3,25 @@ from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
+
+class BaseAbstractModel(models.Model):
+
+    description = models.TextField("Description")
+
+    first_created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class BaseTagAbstractModel(models.Model):
+    content_type = models.ForeignKey(ContentType,
+                                     related_name="content_type_set_for_%(class)s",
+                                     on_delete=models.CASCADE
+                                     )
+    object_pk = models.TextField("Object ID")
+    content_object = GenericForeignKey(ct_field=content_type, fk_field=object_pk)
+
+    class Meta:
+        abstract = True
